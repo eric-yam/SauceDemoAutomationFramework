cab6fd1949c0a4d41829842381b280029795576a
ericyam-git <70084832+ericyam-git@users.noreply.github.com>
2024-12-06 17:15:32 -0500

    Implemented Home Page Feature file, Home Page step definitions

--- src/main/java/StepDefinitions/HomeSteps.java
+++ src/main/java/StepDefinitions/HomeSteps.java
@@ -1,9 +1,14 @@
 package StepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.PageObjects.Pages.HomePage.HomePage;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,14 +17,6 @@ public class HomeSteps {
     TestContext context;
 
     String[] expectedProductNames = {
-            "Sauce Labs Backpack",
-            "Sauce Labs Bike Light",
-            "Sauce Labs Bolt T-Shirt",
-            "Sauce Labs Fleece Jacket",
-            "Sauce Labs Onesie"
-    };
-
-    String[] productsToSelect = {
             "Sauce Labs Backpack",
             "Sauce Labs Bike Light",
             "Sauce Labs Bolt T-Shirt",
@@ -41,7 +38,7 @@ public class HomeSteps {
         this.context.cleanUp();
     }
 
-    @Then("Home Page is Displayed")
+    @And("Home Page is Displayed")
     public void landedHomePage() {
         HomePage homePage = new HomePage(this.context.driver);
         homePage.waitForHomePage();
@@ -53,5 +50,43 @@ public class HomeSteps {
         }
 
         assertTrue(productFound);
+    }
+
+    @And("Add Products To Cart On Home Page")
+    public void addToCart(DataTable dataTable) {
+        List<String> productsToSelect = dataTable.asList();
+
+        HomePage homePage = new HomePage(this.context.driver);
+        assertTrue(homePage.topNavigationBarDisplayed());
+
+        for (String s : productsToSelect) {
+            homePage.clickProductAddToCart(s);
+        }
+    }
+
+    @When("User Applies Filter On Home Page And Validates Filter Applied")
+    public void applyFilter(DataTable dataTable) {
+        List<String> filterOptionList = dataTable.asList();
+
+        HomePage homePage = new HomePage(this.context.driver);
+
+        for (String filterOption : filterOptionList) {
+            homePage.applyFilter(filterOption);
+            homePage.setOrderOfProductList();
+
+            if (filterOption.equals("Name (A to Z)")) {
+                homePage.orderByNameAscend();
+                assertTrue(homePage.orderByNameAscend());
+            } else if (filterOption.equals("Name (Z to A)")) {
+                homePage.orderByNameDescend();
+                assertTrue(homePage.orderByNameDescend());
+            } else if (filterOption.equals("Price (low to high)")) {
+                homePage.orderByPriceAscend();
+                assertTrue(homePage.orderByPriceAscend());
+            } else if (filterOption.equals("Price (high to low)")) {
+                homePage.orderByPriceDescend();
+                assertTrue(homePage.orderByPriceDescend());
+            }
+        }
     }
 }
--- src/test/java/resources/features/Home.feature
+++ src/test/java/resources/features/Home.feature
@@ -6,3 +6,27 @@ Feature: Home Page Tests
       | standard_user | secret_sauce |
     And Validates user successfully logged in
     And Home Page is Displayed
+
+  Scenario: Validate Home Page Filter
+    Given User Logs In:
+      | standard_user | secret_sauce |
+    And Validates user successfully logged in
+    And Home Page is Displayed
+    When User Applies Filter On Home Page And Validates Filter Applied
+      | Name (A to Z)       |
+      | Name (Z to A)       |
+      | Price (low to high) |
+      | Price (high to low) |
+
+  Scenario: Add Products To Cart From Home Page
+    Given User Logs In:
+      | standard_user | secret_sauce |
+    And Validates user successfully logged in
+    And Home Page is Displayed
+
+    Then Add Products To Cart On Home Page
+      | Sauce Labs Backpack      |
+      | Sauce Labs Bike Light    |
+      | Sauce Labs Bolt T-Shirt  |
+      | Sauce Labs Fleece Jacket |
+      | Sauce Labs Onesie        |
